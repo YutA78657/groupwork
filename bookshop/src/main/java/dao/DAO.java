18c0c08d0439e350f3cd91c78ec57ec75a5dae0c
mid <mid@KCSK4123.k401.kcsk.ac.jp>
2026-01-19 11:48:10 +0900

    DAO.java修正

--- bookshop/src/main/java/dao/DAO.java
+++ bookshop/src/main/java/dao/DAO.java
@@ -1,24 +1,18 @@
 package dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-
-
 public class DAO {
-	protected Connection getconnection() throws Exception{
 	//データベース接続に使用する情報
-		String JDBC_URL = "jdbc:mysql://localhost/example";
-		String DB_USER = "root";
-		String DB_PASS = "";
-		
-			//JDBCドライバを読み込む
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-			} catch(ClassNotFoundException e) {
-				throw new IllegalStateException(
-				"JDBCドライバを読み込みませんでした");
-			}
-			//データベースへ接続
-			return DriverManager.getConnection(JDBC_URL,DB_USER,DB_PASS);
+	protected String JDBC_URL = "jdbc:mysql://localhost/example";
+	protected String DB_USER = "root";
+	protected String DB_PASS = "";
+	
+	public void load() throws Exception{
+		//JDBCドライバを読み込む
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch(ClassNotFoundException e) {
+			throw new IllegalStateException(
+			"JDBCドライバを読み込みませんでした");
+		}
 	}
 }
